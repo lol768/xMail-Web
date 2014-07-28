259c9d65d39cf5920994280856fbe03c53d5963c
Travis Ralston <travpc@gmail.com>
2014-07-28 00:41:38 -0600

    Include censored configuration files

---
+++ app/config/xmail.php
@@ -0,0 +1,12 @@
+<?php
+return array(
+    // Secret used for securing portions of the API
+    "api_secret" => "CHANGE_ME",
+    
+    // The number of rounds to hash (higher = longer hash time)
+    "hash_rounds" => 14,
+    
+    // The admin email to use for the contact form
+    "admin_email" => "system@xmail.turt2live.com"
+);
+?>
