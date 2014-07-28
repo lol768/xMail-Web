fde323b0a5116cedb540eb1ce9e3035f683f91a5
Travis Ralston <travpc@gmail.com>
2014-07-28 00:14:23 -0600

    More mailbox mockup

--- app/controllers/MailController.php
+++ app/controllers/MailController.php
@@ -12,9 +12,10 @@ class MailController extends BaseController {
         $mailbox[1] = array("ID" => 2, "from" => "Hawkfalcon", "unread" => true, "sent" => "4 hours ago", "message" => $this->message());
         $mailbox[2] = array("ID" => 3, "from" => "drtshock", "unread" => true, "sent" => "10:00 AM", "message" => $this->message());
         $mailbox[3] = array("ID" => 4, "from" => "FlamingPaw", "unread" => true, "sent" => "Yesterday", "message" => $this->message());
-        $mailbox[4] = array("ID" => 5, "from" => "turt2live@turt2live.com", "unread" => true, "sent" => "July 5", "message" => $this->message());
-        $mailbox[5] = array("ID" => 6, "from" => "Notch", "unread" => false, "sent" => "January 13", "message" => $this->message());
-        $mailbox[6] = array("ID" => 7, "from" => "Dinnerbone", "unread" => false, "sent" => "Last year", "message" => $this->message());
+        $mailbox[4] = array("ID" => 5, "from" => "turt2live@turt2live.com", "unread" => false, "sent" => "July 5", "message" => $this->message());
+        $mailbox[5] = array("ID" => 6, "from" => "travpc@gmail.com", "unread" => true, "sent" => "June 13", "message" => $this->message());
+        $mailbox[6] = array("ID" => 7, "from" => "Notch", "unread" => false, "sent" => "January 13", "message" => $this->message());
+        $mailbox[7] = array("ID" => 8, "from" => "Dinnerbone", "unread" => false, "sent" => "Last year", "message" => $this->message());
 
         // TODO: Make this not happen...
         $maxLength = 50;
@@ -26,19 +27,24 @@ class MailController extends BaseController {
             }
             
             $mailbox[$i]["message"] = $message;
-            $mailbox[$i] = $this->GetHead($mailbox[$i]);
+            $mailbox[$i] = $this->getHead($mailbox[$i]);
         }
         
         return Response::json($mailbox);
     }
     
-    private function GetHead($mailMessage){
+    private function getHead($mailMessage){
         $username = $mailMessage["from"];
         if(strpos($username, '@') === FALSE){
             // Likely a valid minecraft username
             $mailMessage["head"] = "https://minotar.net/avatar/".$username."/50";
         }else{ 
-            $mailMessage["head"] = "images/head_placeholder.png";
+            // Assuming email... if wer're wrong then at least gravatar will give us a generic icon to use.
+            $gravatarhash = md5(strtolower(trim($username)));
+            $mailMessage["head"] = "http://www.gravatar.com/avatar/{$gravatarhash}?r=g&d=mm&s=50";
+            
+            // If gravatar support is ever removed (user setting?)
+            //$mailMessage["head"] = "images/head_placeholder.png";
         }
         return $mailMessage;
     }
--- app/views/mailbox.blade.php
+++ app/views/mailbox.blade.php
@@ -4,22 +4,36 @@
     <link rel="stylesheet" href="{{ URL::asset('css/mailbox.css') }}" />
 @stop
 @section('scripts')
-    <script src="{{ URL::asset('js/mailbox.js') }}"></script>
     <script type="text/javascript">
         var mailUrl = "{{ URL::route('getMail') }}";
+        var loadingRow = "<tr><td class='loading-row'><img src=\"{{ URL::asset('images/loading.png') }}\" class='loading'></td></tr>";
     </script>
+    <script src="{{ URL::asset('js/mailbox.js') }}"></script>
 @stop
 
 @section('content')
     <div class="post">
-        <span class="post-title">Mailbox</span>
+        <span class="post-title">Mailbox <div id="newmessages">You have new messages. Click here to show them</div></span>
         <div class="post-content" style="margin-top: 15px;">
             <table id="mailbox">
                 <tbody>
                 <!-- Populated by JavaScript -->
+                <tr><td class='loading-row'><img src="{{ URL::asset('images/loading.png') }}" class='loading'></td></tr>
                 </tbody>
             </table>
-            <div style="color: #444;text-align: center; width: 100%; padding-bottom: 10px; padding-top: 10px;">no more messages</div>
+            <div style='padding-top: 10px;'>
+                <div id="showing">Showing messages 1 to 10 of 33</div>
+                <div id="pagination">
+                    <span class='btn previous'>&lt;&lt; Previous</span>
+                    <span class='btn' data-page='1'>1</span>
+                    <span class='btn' data-page='2'>2</span>
+                    <span class='btn current' data-page='3'>3</span>
+                    <span class='btn' data-page='4'>4</span>
+                    <span class='btn' data-page='5'>5</span>
+                    <span class='btn next disabled'>Next &gt;&gt;</span>
+                </div>
+                <div class="clear"></div>
+            </div>
         </div>
     </div>
 @stop
